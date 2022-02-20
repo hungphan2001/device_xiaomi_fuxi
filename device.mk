f2aae8bba6d9350a09346c19f149678a8a709826
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    fuxi: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- device.mk
+++ device.mk
@@ -456,6 +456,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.2.vendor
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
