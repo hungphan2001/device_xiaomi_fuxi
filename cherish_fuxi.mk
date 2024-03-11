2619feabe2e7fedc2ba970af3162146f7c221d51
Hung Phan <phandinhhungvp2001@gmail.com>
2024-03-11 14:38:03 +0000

    fuxi:bring cherish
    Signed-off-by: Hung Phan <phandinhhungvp2001@gmail.com>

--- cherish_fuxi.mk
+++ cherish_fuxi.mk
@@ -9,20 +9,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common LineageOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/fuxi/device.mk)
 
-# Inherit from Gapps
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-
 ## Device identifier
 PRODUCT_DEVICE := fuxi
-PRODUCT_NAME := lineage_fuxi
+PRODUCT_NAME := cherish_fuxi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2211133G
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Cherish
+CHERISH_BUILD_TYPE := OFFICIAL
+CHERISH_MAINTAINER := hungphan2001
+CHERISH_CHIPSET := kalama
+CHERISH_BATTERY := 4500mAh
+CHERISH_DISPLAY := 1080x2400
+
+EXTRA_UDFPS_ANIMATIONS := true
 
 PRODUCT_SYSTEM_NAME := 2211133G
 PRODUCT_SYSTEM_DEVICE := 2211133G
